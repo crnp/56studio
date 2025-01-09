b18c7fbc18bc12193e44c3cf7725a2c1ceb4212d
calvinrnp <calvinramadhan@outlook.com>
2025-01-10 03:07:39 +0700

    finished design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
+  // future: {
+  //   hoverOnlyWhenSupported: true,
+  // },
   theme: {
     fontFamily: {
       display: ['Squada One', 'system-ui', 'sans-serif'],
@@ -26,16 +26,41 @@ export default {
       },
     },
     extend: {
+      screens: {
+        'true-hover': { raw: '(hover: hover)' },
+      },
       animation: {
         'infinite-scroll': 'infinite-scroll 15s linear infinite',
+        'spin-slow': 'spin 3s linear infinite',
+        progress: 'progress 1.5s infinite linear',
+        'fade-out': 'fade-out 0.5s forwards',
       },
       keyframes: {
         'infinite-scroll': {
           from: { transform: 'translateX(0)' },
           to: { transform: 'translateX(-100%)' },
         },
+        progress: {
+          '0%': { transform: ' translateX(0) scaleX(0)' },
+          '20%': { transform: ' translateX(0) scaleX(0)' },
+          '40%': { transform: 'translateX(0) scaleX(0.4)' },
+          '100%': { transform: 'translateX(100%) scaleX(0.5)' },
+        },
+        'fade-out': {
+          '100%': {
+            opacity: 0,
+            display: 'none',
+          },
+        },
+      },
+      transformOrigin: {
+        'left-right': '0% 50%',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant('true-hover', '@media (hover: hover)');
+    },
+  ],
 };
